145e99d306319b398872b479fdac5073b484dee1
Habby Whyte <bahybaj@hotmail.com>
2016-03-30 14:44:22 -0400

    added click count-still working on it

--- script.js
+++ script.js
@@ -14,23 +14,90 @@ var language = ["java", "perl", "js", "rails", "ruby", "python", "jquery", "php"
 
 var circles = document.querySelectorAll(".circle");
 console.log(circles)
-var circlesValue = [];
-var firstCircle = '';
-var secondCircle = '';
-var match = 0;
+var startButton = document.querySelector("#start");
+var clicks;
+var clicks = 0;
+var click1 = '';
+var click2 = '';
+var match = '';
 
 
+//To begin the game the player must push the start button to initiate game
+startButton.addEventListener("click", clickCircle);
+
+//setting the game by shuffling cards
+// function shuffle (language) {
+//   for (var i = 0; i < circles.length; i++) {
+//     output
+//   }
+
+
+function showCard(){
 for (var i = 0; i < circles.length; i++) {
   tempCircle = circles[i];
-  console.log(tempCircle);
-  tempCircle.setAttribute("data-language",language[i])
+
+  tempCircle.setAttribute("data-language",language[i]);
+  tempCircle.addEventListener("click", function(){
+    this.innerHTML = this.getAttribute("data-language"),
+      this.style.background = "#33334d";
+  })
+}
+}
+
+
+function clickCircle (tempCircle) {
+  // if (clicks == 2) {return ;}
+  if (clicks == 0){
+  click1 = tempCircle;
+  clicks = 1;
+  showCard();
+  console.log("click")
+  }
+  else {
+    clicks = 2;
+    click2 = tempCircle;
+    showCard();
+  }
+}
+
+  // turns = 1;
+  // var clicks = 0;
+  // click1 = tempCircle;
+  //
+  // if (clicks == 1) { return tempCircle;
+  // }
+  // if (clicks == 2) {
+  //   click2 = tempCircle;
+  //   return tempCircle;
+  //   match = 1
+    // time= setInterval("match()",1000);
+  // }
+
+
+// function match() {
+//   clearInterval (time);
+//   if (language[click1] ==language[click2]) {
+//
+//   }
+// }
+// function clickCircle () {
+//   var clicks = 0;
+//     if (++clicks % 2 == 0) {
+//       console.log(clicks);
+//       clicks = [];
+//       showCards ();
+//   }
+//     else {
+//       console.log("odd");
+//
+//     }
+//   }
+// }
+
+
+// function flippedCircle (tempCircle, i) {
+    // var unflippedCircle = tempCircle.style.background
+    // tempCircle.style.background = "#14141f"
+
 
   // var random = language[Math.floor(Math.random() * language.length)];
-}
-function clickCircle () {
-  if (clicks==2) {
-    return;
-  }
-  if (clicks==0) {
-}
-}
