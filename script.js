7034b8bfb346924b213060578d5eac8b8e9cc190
Habby Whyte <bahybaj@hotmail.com>
2016-03-31 11:04:43 -0400

    added compareCard and flipBack functions- no delay yet

--- script.js
+++ script.js
@@ -15,61 +15,80 @@ var language = ["java", "perl", "js", "rails", "ruby", "python", "jquery", "php"
 var circles = document.querySelectorAll(".circle");
 console.log(circles)
 var startButton = document.querySelector("#start");
-var clicks;
 var clicks = 0;
-var click1 = '';
-var click2 = '';
+var clickCircle1;
+var clickCircle2;
 var match = '';
 
 
 //To begin the game the player must push the start button to initiate game
-startButton.addEventListener("click", clickCircle);
-
-//setting the game by shuffling cards
-// function shuffle (language) {
-//   for (var i = 0; i < circles.length; i++) {
-//     output
-//   }
+// startButton.addEventListener("click", clickCircle);
 
 
-function showCard(){
 for (var i = 0; i < circles.length; i++) {
   tempCircle = circles[i];
 
   tempCircle.setAttribute("data-language",language[i]);
-  tempCircle.addEventListener("click", function(){
-    this.innerHTML = this.getAttribute("data-language"),
-      this.style.background = "#33334d";
-  })
+
+  tempCircle.addEventListener("click", showCard);
 }
+
+function showCard() {
+
+  this.innerHTML = this.getAttribute("data-language"),
+  this.style.background = "#33334d";
+  // increase clicks
+  // if it's an odd click
+    // set click circle 1
+  // if it's an even click
+    // set click circle 2
+    // compare cards
+    // change clickCircles to (null or undefined)
+
+  clickCircle1 = this;
+  compareCard();  //
+  clickCircle2 = this;
+}
+
+
+function compareCard () {
+  clicks++
+
+// if (clicks == 2) {return ;}
+  if (clicks % 2 == 0){
+
+    //get the language of click1
+    clickCircleLanguage1 = clickCircle1.getAttribute("data-language");
+    console.log(clickCircleLanguage1);
+
+    //get the lanaguage of click2
+    clickCircleLanguage2 = clickCircle2.getAttribute("data-language");
+    console.log(clickCircleLanguage2);
+
+    console.log(clickCircleLanguage1 == clickCircleLanguage2);
+
+    //compare the language of click1 and click2
+    //if two cards are the same- do nothing
+    //if two cards are not the same - flip back
+      //change background color back
+      //change innerHTML to empty string
+      if (clickCircleLanguage1 != clickCircleLanguage2) {
+        // setTimeout(flipBack, 700);
+        flipBack()
+      }
+    }
+}
+
+function flipBack() {
+  clickCircle1.style.background = "#e0e0eb";
+  clickCircle2.style.background = "#e0e0eb";
+  clickCircle1.innerHTML = "";
+  clickCircle2.innerHTML = "";
 }
 
 
-function clickCircle (tempCircle) {
-  // if (clicks == 2) {return ;}
-  if (clicks == 0){
-  click1 = tempCircle;
-  clicks = 1;
-  showCard();
-  console.log("click")
-  }
-  else {
-    clicks = 2;
-    click2 = tempCircle;
-    showCard();
-  }
-}
 
-  // turns = 1;
-  // var clicks = 0;
-  // click1 = tempCircle;
-  //
-  // if (clicks == 1) { return tempCircle;
-  // }
-  // if (clicks == 2) {
-  //   click2 = tempCircle;
-  //   return tempCircle;
-  //   match = 1
+
     // time= setInterval("match()",1000);
   // }
 
